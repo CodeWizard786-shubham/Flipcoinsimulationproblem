3e8a3ad2aebcb1614ab2fe7317ce0a7288e2d2d7
shubham shirke <shirkeshubham84@gmail.com>
2023-02-04 18:19:05 +0530

    Added : Perform multiple flips until 21 terms and show win or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo -n "Enter number of Flips: "
-read n
-
+count=0
 head_count=0
 tail_count=0
 Head=0
 tail=0
-for((i=1;i<=n;i++))
+while [ $Head -lt 21 ] && [ $Tail -lt 21]
 do
     Flipcoin=$(($RANDOM%2))
     while [ $Flipcoin -eq 0 ]
@@ -17,8 +15,22 @@ do
     done
     echo "Tails"
     Tail=$(($tail_count+1))
-    
+    while [ $Head -eq 21 ] -o [ $Tail -eq 21 ]
+    do 
+      exit
+    done
+    if [ $Head -gt $Tail ]
+    then 
+      echo "Head win "
+    elif [ $Tail -gt $Head ]
+    then
+      echo "Tail win "
+    else
+      echo "Tie" 
+    fi 
+    count++;   
 done
+
 
 echo "The number of times head appear is: "$Head
 echo "The number of times Tail appear is: "$Tail
