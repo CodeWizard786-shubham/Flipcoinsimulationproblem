4f4832d695474ed0cf8ddc899a0164b50fad6be6
shubham shirke <shirkeshubham84@gmail.com>
2023-02-04 17:54:18 +0530

    Added : Multiple flipping a coin on use user input

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+echo -n "Enter number of Flips: "
+read n
 
-Flipcoin=$(($RANDOM%2))
+head_count=0
+tail_count=0
+Head=0
+tail=0
+for((i=1;i<=n;i++))
+do
+    Flipcoin=$(($RANDOM%2))
+    while [ $Flipcoin -eq 0 ]
+    do
+    echo "Heads"
+    Head=$(($head_count+1))
+    done
+    echo "Tails"
+    Tail=$(($tail_count+1))
+    
+done
 
-if [ $Flipcoin -eq 0 ]
-then
-  echo "Heads"
-else
-  echo "Tails"
-fi
+echo "The number of times head appear is: "$Head
+echo "The number of times Tail appear is: "$Tail
